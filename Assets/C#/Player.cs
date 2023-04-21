6b8b2c9375477221a21c188bafe14e2165b0d23c
Park Yeon Woo <jd05183@naver.com>
2023-04-21 21:12:08 +0900

    230421_update_01
    +Player.cs

---
+++ Assets/C#/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float h;
+    float v;
+
+    Rigidbody2D rigid;
+
+    void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        rigid.velocity = new Vector2(h, v);    
+    }
+}
